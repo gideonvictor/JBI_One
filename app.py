b4f4b9fd797ed1c234f24d4fd4857a940240aebb
gideonvictor <q+github@gideonvictor.com>
2025-10-17 12:20:57 -0700

    Correcting host and port on app.py

--- app.py
+++ app.py
@@ -445,7 +445,7 @@ def sales_team_delete(sales_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=38291)
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
